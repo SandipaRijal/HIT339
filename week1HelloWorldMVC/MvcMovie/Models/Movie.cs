c85ef1a9767c3dbd64d40cd1a5d43399bac8f724
Sandipa Rijal <sandipa_rizal@hotmail.com>
2020-08-02 16:31:17 +0930

    Part 7: add search to an ASP.NET Core MVC app

--- week1HelloWorldMVC/MvcMovie/Models/Movie.cs
+++ week1HelloWorldMVC/MvcMovie/Models/Movie.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
 
 namespace MvcMovie.Models
 {
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
     public class Movie
     {
         public int Id { get; set; }
